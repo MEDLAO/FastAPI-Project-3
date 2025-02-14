fc5e1065d6bae09f3bdad26fd059013de6c9dc9a
MEDLAO <sofiane.medhouny.laouina@gmail.com>
2025-02-14 15:13:43 +0100

    Edit ydl_opts 2

--- main.py
+++ main.py
@@ -16,6 +16,7 @@ def download_video(url: str):
     try:
         ydl_opts = {"format": "bestvideo+bestaudio",
                     "cookies_from_browser": "chrome",
+                    "user_agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/98.0.4758.102 Safari/537.36"
                     }
         with yt_dlp.YoutubeDL(ydl_opts) as ydl:
             info = ydl.extract_info(url, download=True)  # Download video
