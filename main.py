5a1f7adf8a00ab25d3084c0bfd99b387ff8f8e26
MEDLAO <sofiane.medhouny.laouina@gmail.com>
2025-04-04 10:21:45 +0000

    Edit middleware

--- main.py
+++ main.py
@@ -23,8 +23,8 @@ RAPIDAPI_SECRET = os.getenv("RAPIDAPI_SECRET")
 
 @app.middleware("http")
 async def enforce_rapidapi_usage(request: Request, call_next):
-    # Bypass authentication for the /welcome endpoint
-    if request.url.path == "/":
+    # Allow "/" and "/health" to work without the header
+    if request.url.path in ["/", "/health"]:
         return await call_next(request)
 
     rapidapi_proxy_secret = request.headers.get("X-RapidAPI-Proxy-Secret")
